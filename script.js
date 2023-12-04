8d768075a42c612ca1ae018e159f3e5dec31f65c
Dario <darionas@gmail.com>
2023-12-04 19:28:10 +0200

    modified_4: index.html, script.js, style.css

--- script.js
+++ script.js
@@ -17,36 +17,28 @@ const monthsNumber = document.querySelector('.result__months-number');
 const daysNumber = document.querySelector('.result__days-number');
 const inputs = document.getElementsByTagName('input');
 const btn = document.querySelector('#btn');
-let dayInput, monthInput, yearInput, dateInput;
-//alert(getDay);
+let dateInput;
 
 
-        btn.addEventListener('click',function(e) {
-            e.preventDefault();
-            const today = new Date();
-            const getYear = today.getFullYear();
-            const getMonth = today.getMonth();
-            const getDay = today.getDate();
-            const dayVal = + document.querySelector('#day__id').value;
-            const monthVal = + document.querySelector('#month__id').value -1;
-            const yearVal = + document.querySelector('#year__id').value;      
-            //alert(dayVal > getDay);
-            dateInput = new Date(yearVal, monthVal, dayVal);
-           
-            console.log(yearVal);
-            console.log(getYear);
-            console.log(monthVal);
-            console.log(getMonth);
-            console.log(dayVal);
-            console.log(getDay);
-            
-        for(let i=0; i < inputs.length; i++) {
-            let input = Number(inputs[i].value);
-            //alert(typeof input);
-        if(input === null || input === undefined || input === 0) {
-            labels[i].classList.add('coloring');
-            error[i].classList.add('visible');
-            error[i].innerHTML = 'This field is required';
+btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    const today = new Date();
+    const getYear = today.getFullYear();
+    const getMonth = today.getMonth();
+    const getDay = today.getDate();
+    const dayVal = + day.value;
+    const monthVal = + month.value -1;
+    const yearVal = + year.value;      
+   
+    dateInput = new Date(yearVal, monthVal, dayVal);
+                
+    for(let i=0; i < inputs.length; i++) {
+        let input = Number(inputs[i].value);
+        
+    if(input === null || input === undefined || input === 0) {
+        labels[i].classList.add('coloring');
+        error[i].classList.add('visible');
+        error[i].innerHTML = 'This field is required';
             
         } else if(isNaN(input)) {
             labels[i].classList.add('coloring');
@@ -96,7 +88,9 @@ let dayInput, monthInput, yearInput, dateInput;
         monthError.classList.add('visible');
         monthError.innerHTML = 'Must be a valid month';
         
-    } if(yearVal > getYear) {
+    } 
+    
+    if(yearVal > getYear) {
         dayLabel.classList.add('coloring');
         dayError.classList.add('visible');
         dayError.innerHTML = 'Must be a valid day';
@@ -109,8 +103,56 @@ let dayInput, monthInput, yearInput, dateInput;
         yearError.classList.add('visible');
         yearError.innerHTML = 'Must be in the past';
     }
-    
        
+   
+    let yearResult = getYear - yearVal;
+    let monthResult, dayResult;
+    if(dateInput < today) {
+        if (getMonth < monthVal) {
+            monthResult = getMonth + 12 - monthVal;
+        } else {
+            monthResult = getMonth - monthVal;
+        }
+        if (getDay < dayVal) {
+            dayResult = getDay + 30 - dayVal;
+        } else {
+            dayResult = getDay - dayVal;
+        }
+    
+        if(yearResult > 1 || yearResult == 0) {
+            document.querySelector('.y').classList.add('visible');
+            document.querySelector('.y').classList.remove('hide');
+            yearsNumber.innerHTML = yearResult;
+        } 
+        if(yearResult == 1) {
+            document.querySelector('.y').classList.remove('visible');
+            document.querySelector('.y').classList.add('hide');
+            yearsNumber.innerHTML = yearResult;
+        }
+        
+        if(monthResult > 1 || monthResult == 0) {
+            document.querySelector('.m').classList.add('visible');
+            document.querySelector('.m').classList.remove('hide');
+            monthsNumber.innerHTML = monthResult;
+        } 
+        if(monthResult == 1) {
+            document.querySelector('.m').classList.remove('visible'); 
+            document.querySelector('.m').classList.add('hide');
+            monthsNumber.innerHTML = monthResult;
+        }
+        
+        if(dayResult > 1 || dayResult == 0) {
+            document.querySelector('.d').classList.add('visible');
+            document.querySelector('.d').classList.remove('hide');
+            daysNumber.innerHTML = dayResult;
+        } 
+        if(dayResult == 1) {
+            document.querySelector('.d').classList.remove('visible'); 
+            document.querySelector('.d').classList.add('hide');
+            daysNumber.innerHTML = dayResult;
+        }
+
+    }
 })
 
 
